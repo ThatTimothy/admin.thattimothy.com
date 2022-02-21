52938ea4e033c0b26d07b4c957d44f23e19489f8
ThatTimothy <42754621+ThatTimothy@users.noreply.github.com>
2022-02-21 17:43:43 -0600

    Add grant admin button

--- docs/main.js
+++ docs/main.js
@@ -53,14 +53,33 @@ async function render() {
 
 	options.style.display = "block"
 
+	grantAdmin = document.getElementById("grant_admin")
 	shutdownAllServers = document.getElementById("shutdown_all_servers")
 	migrateToLatestUpdate = document.getElementById("migrate_to_latest_update")
 
+	grantAdmin.addEventListener("click", onGrantAdminClick)
 	shutdownAllServers.addEventListener("click", onShutdownClick)
 	migrateToLatestUpdate.addEventListener("click", onMigrateClick)
 }
 
+function onGrantAdminClick() {
+	grantAdmin = document.getElementById("grant_admin")
+	if (grantAdmin.getAttribute("disabled")) {
+		return
+	}
+
+	let username = prompt("Enter username to grant admin to:")
+
+	if (username.length <= 3 || username.length > 20) {
+		alert("Invalid username")
+		return onGrantAdminClick()
+	}
+
+	showPrompt("Grant Admin", `Are you sure you want to grant admin to '${username}'?`, performGrantAdmin)
+}
+
 function onShutdownClick() {
+	shutdownAllServers = document.getElementById("shutdown_all_servers")
 	if (shutdownAllServers.getAttribute("disabled")) {
 		return
 	}
@@ -69,11 +88,16 @@ function onShutdownClick() {
 }
 
 function onMigrateClick() {
+	migrateToLatestUpdate = document.getElementById("migrate_to_latest_update")
 	if (migrateToLatestUpdate.getAttribute("disabled")) {
 		return
 	}
 
 	showPrompt("Migrate To Latest Update", "Are you sure you want to migrate to the latest update?", performMigrate)
+}
+
+function performGrantAdmin() {
+	performAction("grant_admin")
 }
 
 function performShutdown() {
@@ -92,15 +116,15 @@ function onGameSelectChange() {
 	gameSelect = document.getElementById("game_select")
 	gameSelected = gameSelect.value
 
-	shutdownAllServers = document.getElementById("shutdown_all_servers")
+	options = document.getElementById("options")
 	migrateToLatestUpdate = document.getElementById("migrate_to_latest_update")
 
-	if (gameSelected) {
-		shutdownAllServers.removeAttribute("disabled")
-		migrateToLatestUpdate.removeAttribute("disabled")
-	} else {
-		shutdownAllServers.setAttribute("disabled", true)
-		migrateToLatestUpdate.setAttribute("disabled", true)
+	for (option of options.children) {
+		if (gameSelected) {
+			option.removeAttribute("disabled")
+		} else {
+			option.setAttribute("disabled", true)
+		}
 	}
 }
 
